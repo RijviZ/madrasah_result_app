1f58bbb371bc3af9af6c8a02a0765e29f357251f
Rijvi Zaman <rijvizaman8@gmail.com>
2026-07-23 00:38:43 +0600

    Deploy fixed web build without LateInitializationError

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1909551344" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1411047905" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
