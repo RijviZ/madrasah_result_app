28f9640f790af2bddc466513c92dfd0f571e73d6
Rijvi Zaman <rijvizaman8@gmail.com>
2026-07-23 00:44:44 +0600

    Deploy web app with backup LateInitializationError fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1411047905" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "692159311" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
