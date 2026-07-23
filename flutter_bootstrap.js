d22024b875e51e7308cd3549e75ae118c9715779
Rijvi Zaman <rijvizaman8@gmail.com>
2026-07-23 10:27:54 +0600

    Deploy latest Web release with real-time sync and overflow fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4164627426" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3422997523" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
